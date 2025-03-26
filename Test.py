42fe8b0cd091aa26950aeddb9b039a7627d10f2c
lemarshall1 <Lex82105@gmail.com>
2025-03-26 13:51:40 -0500

    Update Test.py
    This turns off led using motion its just random raspberry pi code

--- Test.py
+++ Test.py
@@ -1,1 +1,9 @@
-print('test')
+from gpiozero import LED, Button, Buzzer, MotionSensor, DistanceSensor
+from signal import pausein
+pir = MotionSensor(4) #pin number? i think
+led = LED(16)
+
+pir.when_motion = led.on
+pir.when_no_motion = led.off
+
+pause
